4a1fcb4e62da81641fb8e3bf719c87d6f5024f79
radhad-ux <radhad83@gmail.com>
2021-03-07 16:19:51 +0100

    Main method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,48 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    ToDoList toDoList;
+    String filename = "src/main/resources/todolist.txt";
+    private ArrayList<Task> taskList;
+
+     public Main(ArrayList<Task> taskList, ToDoList toDoList) {
+         this.taskList = taskList;
+         this.toDoList = toDoList;
+    }
+     public void displayMainMenu(){
+             int choice = 0;
+             boolean endTask = false;
+             Scanner input = new Scanner(System.in);
+             System.out.println("    Hello! Welcome to your To do List      ");
+             System.out.println("-------------------------------------------");
+             System.out.println("     1- Display List      ");
+             System.out.println("     2- Add New Task      ");
+             System.out.println("     3- Edit Task         ");
+             System.out.println("     4- Delete Task       ");
+             System.out.println("     5- Update & Exit     ");
+             System.out.println("-------------------------------------------");
+             System.out.println(" Enter correct option");
+             choice = input.nextInt( );
+             while (!(endTask)) {
+
+                 if (choice == 1) {
+                     displayTask( );
+                 }
+                 if (choice == 2) {
+                     addTask( );
+                 }
+                 if (choice == 3)
+                     editTask( );
+                 if (choice == 4)
+                     saveAndExitTask( );
+             }
+         }
+
+
+
     public static void main(String[] args) {
+
 
     }
 }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -9,10 +9,10 @@ public class ToDoList {
     // Constructor to create a ToDo list
 
 
-    public ToDoList(ArrayList<Task> taskList) {
+    //public ToDoList(ArrayList<Task> taskList) {
 
-        this.taskList = taskList;
-    }
+        //this.taskList = taskList;
+    //}
     // Method to return the number of tasks done
 
     public int getNumberOfTasksDone() {
@@ -24,34 +24,6 @@ public class ToDoList {
         return count;
     }
      // Method to display the main menu
-    public void displayMainMenu() {
-        int choice = 0;
-        boolean endTask = false;
-        Scanner input = new Scanner(System.in);
-        System.out.println("    Hello! Welcome to your To do List      ");
-        System.out.println("-------------------------------------------");
-        System.out.println("     1- Display List      ");
-        System.out.println("     2- Add New Task      ");
-        System.out.println("     3- Edit Task         ");
-        System.out.println("     4- Delete Task       ");
-        System.out.println("     5- Update & Exit     ");
-        System.out.println("-------------------------------------------");
-        System.out.println(" Enter correct option");
-        choice = input.nextInt( );
-        while (!(endTask)) {
-
-            if (choice == 1) {
-                displayTask( );
-            }
-            if (choice == 2) {
-                addTask( );
-            }
-            if (choice == 3)
-                editTask( );
-            if (choice == 4)
-                saveAndExitTask( );
-        }
-    }
 
           /** Method to display task by projectName or Due date */
 
@@ -209,6 +181,7 @@ public class ToDoList {
     }
 
     private void saveAndExitTask() {
+        System.out.println("");
 
     }
 
