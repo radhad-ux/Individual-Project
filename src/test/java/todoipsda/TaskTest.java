1a039c968860d14bfb721dbc422e95fd15a8d01a
radhad-ux <radhad83@gmail.com>
2021-03-19 19:11:52 +0100

    Documentation Added

--- src/test/java/todoipsda/TaskTest.java
+++ src/test/java/todoipsda/TaskTest.java
@@ -1,13 +1,12 @@
 package todoipsda;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskTest {
 
@@ -17,8 +16,7 @@ public class TaskTest {
     LocalDate validDueDate = LocalDate.parse("2021-04-13");
 
     @BeforeEach
-    public void setUp()
-    {
+    public void setUp() {
         task = new Task(validTitle, validDueDate, validProject);
     }
 
@@ -27,22 +25,21 @@ public class TaskTest {
         Task task1 = new Task(validTitle, validDueDate, validProject);
         assertThrows(NullPointerException.class, ()-> task1.setTitle(""));
     }
+
     @Test
     public void testNotEmptyTitle() {
         Task task1 = new Task(validTitle, validDueDate, validProject);
         assertDoesNotThrow(()-> task1.setTitle("Java"));
     }
 
-
-   @Test
+    @Test
     public void testValidProjectName(){
-
         assertEquals(validProject, task.getProjectName());
     }
+
     @Test
     public void testValidDueDate(){
-
         assertEquals(validDueDate, task.getDueDate());
     }
-
 }
+
--- src/test/java/todoipsda/ToDoListTest.java
+++ src/test/java/todoipsda/ToDoListTest.java
@@ -1,11 +1,11 @@
 package todoipsda;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-
 
 public class ToDoListTest {
     ArrayList<Task> taskList;
@@ -14,57 +14,62 @@ public class ToDoListTest {
     LocalDate dueDate;
     String projectName;
 
-
     @BeforeEach
-    public void setUp()
-    {
+    public void setUp() {
         taskList = new ArrayList<Task>( );
         toDoList = new ToDoList(taskList);
-        //taskList = new Task(title, dueDate, projectName);
     }
 
     @Test
-    public void testAddTaskCorrectSize(){
-        ArrayList<Task> taskList = new ArrayList<Task>();
+    /** Pssitive test to check the size of the todo list is equal to the
+     * number of tasks added to the Array list
+     * */
+    public void testAddTaskCorrectSize() {
+        ArrayList<Task> taskList = new ArrayList<Task>( );
         ToDoList toDoList = new ToDoList(taskList);
         toDoList.title = "Validate";
         toDoList.dueDate = LocalDate.parse("2021-04-13");
         toDoList.projectName = "project";
-        toDoList.addNewTask();
-        Assertions.assertEquals(1, toDoList.getSize());
+        toDoList.addNewTask( );
+        Assertions.assertEquals(1, toDoList.getSize( ));
     }
 
     @Test
-    public void testAddTaskWrongSize()
-    {
-        ArrayList<Task> taskList = new ArrayList<Task>();
+    /** Negative test to check the size of the todo list is equal to the
+     * number of tasks added to the Array list
+     * */
+    public void testAddTaskWrongSize() {
+        ArrayList<Task> taskList = new ArrayList<Task>( );
         ToDoList toDoList = new ToDoList(taskList);
         toDoList.title = "Validate";
         toDoList.dueDate = LocalDate.parse("2021-04-13");
         toDoList.projectName = "project";
-        toDoList.addNewTask();
-        Assertions.assertNotEquals(21, toDoList.getSize());
+        toDoList.addNewTask( );
+        Assertions.assertNotEquals(21, toDoList.getSize( ));
     }
+
     @Test
+    /** Test to check the number of tasks done is equal to the number of tasks
+     * completed in the taskList
+     * */
     public void testToGetNumberOfTasksDone() {
-
-        Task task = new Task("Test Title",LocalDate.parse("2021-04-13"),"Test Project");
+        Task task = new Task("Test Title", LocalDate.parse("2021-04-13"), "Test Project");
         task.setIsTaskDone(true);
         taskList.add(task);
-        toDoList.addNewTask();
-
-        Assertions.assertEquals(1, toDoList.getNumberOfTasksDone());
+        toDoList.addNewTask( );
+        Assertions.assertEquals(1, toDoList.getNumberOfTasksDone( ));
     }
 
     @Test
+    /** Test to check the number of tasks done is 0 if the number of tasks
+     * completed in the taskList is false
+     *
+     * */
     public void testZeroTasksDone() {
-        Task task = new Task("Test Title",LocalDate.parse("2021-04-13"),"Test Project");
+        Task task = new Task("Test Title", LocalDate.parse("2021-04-13"), "Test Project");
         task.setIsTaskDone(false);
         taskList.add(task);
-        toDoList.addNewTask();
-
-        Assertions.assertEquals(0, toDoList.getNumberOfTasksDone());
+        toDoList.addNewTask( );
+        Assertions.assertEquals(0, toDoList.getNumberOfTasksDone( ));
     }
-
-     
 }
